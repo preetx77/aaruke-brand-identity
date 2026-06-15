40778f052e2142c1a55f15320d48d01a2cae2638
Preet <83566702+preetx77@users.noreply.github.com>
2026-06-15 14:13:24 +0530

    Add AarukeAIChatbot to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AarukeAIChatbot } from "@/components/AarukeAIChatbot";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
@@ -19,6 +20,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <AarukeAIChatbot />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
